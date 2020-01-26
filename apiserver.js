365018a894da1e418d06bee01fc1c9d19f138643
root <root@BlockchainMV.afymqiehnqkeleosmd1jg31ymg.ax.internal.cloudapp.net>
2020-01-26 02:24:59 +0000

    Server finished

--- apiserver.js
+++ apiserver.js
@@ -166,15 +166,60 @@ app.post('/api/addship', async function (req, res) {
             req.body.fechafab, req.body.materiales, req.body.descripcion, req.body.cantidad, req.body.precioud,
             req.body.preciotot, req.body.org, req.body.dst, req.body.ordenante, req.body.fechaenv);
         console.log('Transaction has been submitted');
-        res.send('Transaction has been submitted');
+        res.status(200).json({response : 'Transaction has been submitted'});
 
         // Disconnect from the gateway.
         await gateway.disconnect();
 
     } catch (error) {
         console.error(`Failed to submit transaction: ${error}`);
+        res.status(500).json({ error: error });
     }
+});
 
+app.post('/api/editship', async function (req, res) {
+    ccpPath = path.resolve(__dirname, '.', 'connection-org1.json');
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const adminExists = await wallet.exists('Admin@org1.example.com');
+        if (!adminExists) {
+            console.log('An identity for the user "admin" does not exist in the wallet');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'Admin@org1.example.com', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork(req.body.channel);
+
+        // Get the contract from the network.
+        const contract = network.getContract('shipcc');
+
+        // Submit the specified transaction.
+        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+        console.log(req.body);
+        await contract.submitTransaction('edit', req.body.key, req.body.producto, req.body.modelo, req.body.tipo, req.body.dimensiones,
+            req.body.fechafab, req.body.materiales, req.body.descripcion, req.body.cantidad, req.body.precioud,
+            req.body.preciotot, req.body.org, req.body.dst, req.body.ordenante, req.body.fechaenv);
+        console.log('Transaction has been submitted');
+        res.status(200).json({response : 'Transaction has been submitted'});
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        res.status(500).json({ error: error });
+    }
 });
 
 https.createServer({

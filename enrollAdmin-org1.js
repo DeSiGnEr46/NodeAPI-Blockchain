53d4ee345b113e71b7c17eff5378fcf9a9629be5
root <root@BlockchainMV.afymqiehnqkeleosmd1jg31ymg.ax.internal.cloudapp.net>
2020-01-27 13:46:37 +0000

    Fixed minor bug

--- enrollAdmin-org1.js
+++ enrollAdmin-org1.js
@@ -35,14 +35,14 @@ async function main() {
             // Get the contract from the network.
             const contract = network.getContract('shipcc');
             //const result = await contract.submitTransaction('set','2', 'Tuerca', 'MNB4', 'Hexagonal', '2mmX2mmX1mm', '24-01-2020',
-            //'Hierro', 'Tuerca compatible con tornillo modelo XC32', '4500', '0.02', '80', 'Org1', 'Org3', 'Oliver', '26-01-2020');
+            //'Hierro', 'Tuerca compatible con tornillo modelo XC32', '4500', '0.02', '80', 'Org1', 'org1', 'Oliver', '26-01-2020');
             const result = await contract.evaluateTransaction('get','2');
             console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
             return;
         }
         // Enroll the admin user, and import the new identity into the wallet.
         const cert = fs.readFileSync('/home/saw_vi/Documents/fabric-samples/3org2ch_143/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem').toString();
-        const key = fs.readFileSync('/home/saw_vi/Documents/fabric-samples/3org2ch_143/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/9edd8a650c6fb66e78f3338fe9333f42971c8e6498ad19ea1fcf54f898d14b89_sk').toString();
+        const key = fs.readFileSync('/home/saw_vi/Documents/fabric-samples/3org2ch_143/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/4e329156ff60dd807d5ce6d2eddd6a7d4c8f59beda0917715b63af7225f819c4_sk').toString();
 
         const identityLabel = 'Admin@org1.example.com';
         const identity = X509WalletMixin.createIdentity('Org1MSP', cert, key);
